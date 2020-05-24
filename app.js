a897cbc3893af007200f30e514af6ebfe827d143
Randeep Singh <randeeprattan90@gmail.com>
2020-05-24 14:42:51 +0100

    Load play and pause song

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log("app js");
+const musicContainer = document.getElementById("music-container"),
+  playBtn = document.getElementById("play"),
+  prevBtn = document.getElementById("prev"),
+  nextBtn = document.getElementById("next"),
+  audio = document.getElementById("audio"),
+  progress = document.getElementById("progress"),
+  progressContainer = document.getElementById("progress-container"),
+  title = document.getElementById("title"),
+  cover = document.getElementById("cover"),
+  songs = ["hey", "summer", "ukulele"];
+
+// keep track of song
+let songIndex = 2;
+
+// initialy load song details into DOM
+loadSong(songs[songIndex]);
+
+// update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `./music/${song}.mp3`;
+  cover.src = `./images/${song}.jpg`;
+}
+
+// pause song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+// pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+// event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  isPlaying ? pauseSong() : playSong();
+});
