95d7055a21fe25c0601dbfea81e351997280202c
Randeep Singh <randeeprattan90@gmail.com>
2020-05-24 14:56:01 +0100

    Playback controls, next, prev and progress bar

--- app.js
+++ app.js
@@ -40,8 +40,62 @@ function pauseSong() {
   audio.pause();
 }
 
+// previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+
+  progress.style.width = `${progressPercent}%`;
+}
+
+// set progress bar
+function setProgress(e) {
+  const width = this.clientWidth,
+    clickX = e.offsetX,
+    duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
 // event listeners
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
   isPlaying ? pauseSong() : playSong();
 });
+
+// change song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// time song update event
+audio.addEventListener("timeupdate", updateProgress);
+
+// click on progress bar
+progressContainer.addEventListener("click", setProgress);
+
+// song ends
+audio.addEventListener("ended", nextSong);
